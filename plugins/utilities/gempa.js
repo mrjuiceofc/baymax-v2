9043a1cc2dc5fcbf0ca8dd2beedca5d7ac89276e
mrjuiceofc <133717102+mrjuiceofc@users.noreply.github.com>
2023-08-22 17:49:15 +0200

    baymax v2

---
+++ plugins/utilities/gempa.js
@@ -0,0 +1,29 @@
+exports.run = {
+   usage: ['gempa'],
+   category: 'utilities',
+   async: async (m, {
+      client,
+      Func
+   }) => {
+      try {
+         client.sendReact(m.chat, '🕒', m.key)
+         let json = await Api.neoxr('/gempa')
+         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+         let caption = `乂  *G E M P A*\n\n`
+         caption += `	⌯  *Lintang* : ${json.data.lintang}\n`
+         caption += `	⌯  *Bujur* : ${json.data.bujur}\n`
+         caption += `	⌯  *Skala* : ${json.data.magnitudo}\n`
+         caption += `	⌯  *Kedalaman* : ${json.data.kedalaman}\n`
+         caption += `	⌯  *Waktu* : ${json.data.waktu}\n`
+         caption += `	⌯  *Pusat Gempa* : ${json.data.wilayah}\n\n`
+         caption += global.footer
+         client.sendMessageModify(m.chat, caption, m, {
+            largeThumb: true,
+            thumbnail: await Func.fetchBuffer(json.data.map)
+         })
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false
+}
---
+++ plugins/utilities/igstalk.js
@@ -0,0 +1,37 @@
+exports.run = {
+   usage: ['igstalk'],
+   use: 'username',
+   category: 'utilities',
+   async: async (m, {
+      client,
+      args,
+      isPrefix,
+      command,
+      Func
+   }) => {
+      try {
+         if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'hosico_cat'), m)
+         client.sendReact(m.chat, '🕒', m.key)
+         const json = await Api.neoxr('/igstalk', {
+         	username: args[0]
+         })
+         if (!json.status) return client.reply(m.chat, Func.texted('bold', `🇿🇼 Account not found.`), m)
+         let caption = `乂  *I G - S T A L K*\n\n`
+         caption += `	⌯  *Name* : ${json.data.name}\n`
+         caption += `	⌯  *Username* : ${json.data.username}\n`
+         caption += `	⌯  *Posts* : ${json.data.post}\n`
+         caption += `	⌯  *Followers* : ${json.data.follower}\n`
+         caption += `	⌯  *Followings* : ${json.data.following}\n`
+         caption += `	⌯  *Bio* : ${json.data.about}\n`
+         caption += `	⌯  *Private* : ${Func.switcher(json.data.private, '√', '×')}\n\n`
+         caption += global.footer
+         client.sendFile(m.chat, json.data.photo, 'image.png', caption, m)
+      } catch (e) {
+         return client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false,
+   limit: true,
+   cache: true,
+   location: __filename
+}
