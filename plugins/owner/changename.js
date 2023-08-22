009d5bb4d119b525fe23ac18caec7cc113e2268f
mrjuiceofc <133717102+mrjuiceofc@users.noreply.github.com>
2023-08-22 17:47:25 +0200

    baymax v2

---
+++ plugins/owner/changename.js
@@ -0,0 +1,23 @@
+exports.run = {
+   usage: ['changename'],
+   use: 'text',
+   category: 'owner',
+   async: async (m, {
+      client,
+      text,
+      isPrefix,
+      command,
+      Func
+   }) => {
+      try {
+         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'neoxr bot'), m)
+         if (text.length > 25) return client.reply(m.chat, `🇿🇼 Text is too long, maximum 25 characters.`, m)
+         client.authState.creds.me.name = text
+         await props.save(global.db)
+         return client.reply(m.chat, `🇿🇼 Name successfully changed.`, m)
+      } catch {
+         return client.reply(m.chat, Func.texted('bold', `🇿🇼 Name failed to change.`), m)
+      }
+   },
+   owner: true
+}
